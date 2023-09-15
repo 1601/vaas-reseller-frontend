dd8c960657a96ffe56657c5d65a4a64419aaf49c
GPet <73317823+GemarPetere@users.noreply.github.com>
2023-09-15 15:00:34 +0800

    Update header mobile responsive/MenuIcon function

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,10 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
+import Drawer from '@mui/material/Drawer';
+import Hidden from '@mui/material/Hidden';
+import List from '@mui/material/List'; // Import List component
+import ListItem from '@mui/material/ListItem'; // Import ListItem component
+import ListItemText from '@mui/material/ListItemText';
 import VortexBlackLogo from '../assets/vortex_logo_black.png';
 
-function Header() {
 
+function Header() {
   const [top, setTop] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+
+  const DrawerButtonHandle = (() => {
+    if (isMenuOpen === true) {
+      return <CloseIcon onClick={() => setIsMenuOpen(false)} />
+    }
+    return <MenuIcon onClick={() => setIsMenuOpen(true)} />
+  })
 
   // detect whether user has scrolled the page down by 10px 
   useEffect(() => {
@@ -43,10 +61,50 @@ function Header() {
               <li>
                 <Link to="/login" className="btn text-white bg-gray-900 text-shopifysans font-bold inline-block leading-none overflow-hidden max-w-full transition-all focus-visible:outline-4 focus-visible:outline-focus focus-visible:outline-offset-[-2px] border-solid border-2 rounded-[20px] focus-visible:outline-none tracking-ff-tight border-[transparent] text-white bg-shade-100 hover:bg-shade-70 active:bg-shade-50 disabled:bg-shade-20 disabled:text-shade-30 text-xs md:text-sm px-2 md:px-3 py-1 md:py-2">Create Shop</Link>
               </li>
+              <li>
+                <Hidden mdUp>
+                  {DrawerButtonHandle()}
+                </Hidden>
+              </li>
             </ul>
           </nav>
         </div>
       </div>
+      <Drawer
+        anchor="top"
+        open={isMenuOpen}
+        onClose={() => setIsMenuOpen(false)}
+        className='z-20'
+        PaperProps={{
+          style: {
+            height: '20vh', // Set the desired height here (e.g., 80% of viewport height)
+            justifyContent: 'flex-end',
+            borderBottomLeftRadius: '44px', // Adjust the radius value as needed
+            borderBottomRightRadius: '44px', // Adjust the radius value a
+          },
+        }}
+      >
+        <List
+          sx={{ marginLeft: '32px', marginRight: "32px" }}
+        >
+          <ListItem component={Link} to="/solutions">
+            <ListItemText>
+              <Typography variant="body1" style={{ fontWeight: 'bold' }}>
+                Solutions
+              </Typography> 
+            </ListItemText>
+          </ListItem>
+          <hr />
+          <ListItem component={Link} to="/solutions">
+            <ListItemText>
+              <Typography variant="body1" style={{ fontWeight: 'bold' }}>
+                Pricing
+              </Typography> 
+            </ListItemText>
+          </ListItem>
+          {/* Add more menu items as needed */}
+        </List>
+      </Drawer>
     </header>
 
   );
