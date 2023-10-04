fad0e8d7d8a7155fb8a06da3ea21e5683dd3ec6d
Rver17 <rvabasilio2019@plm.edu.ph>
2023-10-04 12:41:21 +0800

    added userName as backup if no firstLastName

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -14,7 +14,7 @@ const user = JSON.parse(localStorage.getItem('user'));
 const role = localStorage.getItem('role');
 
 const account = {
-  displayName: user ? `${user.firstName} ${user.lastName}` : 'Guest',
+  displayName: user ? (user.firstName && user.lastName ? `${user.firstName} ${user.lastName}` : user.username) : 'Guest',
   designation: user ? user.designation : 'N/A',
   email: user ? user.email : 'guest@email.com',
   photoURL: '/assets/images/avatars/avatar_default.jpg',
