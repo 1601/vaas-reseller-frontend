1473276a83a05f42cf21766bb98af9c730895c5e
Rver17 <rvabasilio2019@plm.edu.ph>
2023-10-05 17:20:34 +0800

    removed designation

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -15,7 +15,6 @@ const role = localStorage.getItem('role');
 
 const account = {
   displayName: user ? (user.firstName && user.lastName ? `${user.firstName} ${user.lastName}` : user.username) : 'Guest',
-  designation: user ? user.designation : 'N/A',
   email: user ? user.email : 'guest@email.com',
   photoURL: '/assets/images/avatars/avatar_default.jpg',
 };
