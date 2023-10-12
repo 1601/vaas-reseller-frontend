4c1ec13172ae1ea7ec3582b0e159a761b4261845
Rver17 <rvabasilio2019@plm.edu.ph>
2023-10-12 14:38:15 +0800

    created Store Data Fetching component

---
+++ src/components/user-account/StoreDataFetch.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const StoreDataFetch = (userId) => {
+  const [storeData, setStoreData] = useState(null);
+  const [editedData, setEditedData] = useState({});
+  const [platformVariables, setPlatformVariables] = useState({});
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchStoreData = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/stores/owner/${userId}`);
+        setStoreData(response.data);
+        setEditedData(response.data);
+        setPlatformVariables(response.data.platformVariables);
+      } catch (err) {
+        console.error('Could not fetch store data', err);
+        setError(err);
+      }
+    };
+    fetchStoreData();
+  }, [userId]);
+
+  return { storeData, editedData, platformVariables, error };
+};
+
+export default StoreDataFetch;
