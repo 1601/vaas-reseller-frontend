bc6cd9242faa4f47c4aea90f75aac771b288cd52
Rver17 <rvabasilio2019@plm.edu.ph>
2023-10-12 14:10:43 +0800

    added comp for fetching userData

---
+++ src/components/user-account/UserDataFetch.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const UserDataFetch = (userId) => {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/user/${userId}`);
+        setUserData(response.data);
+      } catch (error) {
+        console.error('Could not fetch user data', error);
+      }
+    };
+
+    if (userId) {
+      fetchUserData();
+    }
+  }, [userId]);
+
+  return userData;
+};
+
+export default UserDataFetch;
