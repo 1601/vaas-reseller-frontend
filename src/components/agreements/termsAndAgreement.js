c7acdb71adea3a10a345aa2793bdf00874de322d
Rver17 <rvabasilio2019@plm.edu.ph>
2023-10-13 11:54:24 +0800

    added t&c component

---
+++ src/components/agreements/termsAndAgreement.js
@@ -0,0 +1,61 @@
+const termsAndAgreement = `
+These terms and conditions ("Terms") govern your use of [Your Company Name]'s website, products, and services (collectively referred to as the "Service"). By accessing or using the Service, you agree to be bound by these Terms. If you do not agree to these Terms, please do not use the Service.
+
+  1. Acceptance of Terms
+  
+  By using the Service, you agree to abide by these Terms and any future modifications. [Your Company Name] reserves the right to update or change these Terms at any time without notice. Your continued use of the Service after such changes will constitute acceptance of the updated Terms.
+  
+  2. Privacy Policy
+  
+  Your use of the Service is also governed by our Privacy Policy, which can be found [link to Privacy Policy]. Please review this policy to understand how [Your Company Name] collects, uses, and protects your personal information.
+  
+  3. User Registration
+  
+  Some features of the Service may require you to register an account. You agree to provide accurate and complete information during the registration process. You are responsible for maintaining the confidentiality of your account information and password. You agree to notify [Your Company Name] immediately of any unauthorized use of your account.
+  
+  4. Use of the Service
+  
+  You agree to use the Service only for lawful purposes and in compliance with all applicable laws and regulations. You may not:
+  
+  Use the Service in any way that could harm, disable, overburden, or impair [Your Company Name]'s servers or networks.
+  Attempt to gain unauthorized access to any part of the Service or other accounts, computer systems, or networks connected to the Service.
+  Transmit any viruses, worms, or malicious code through the Service.
+  Use the Service to engage in any form of illegal, abusive, or unethical activity.
+  5. Content
+  
+  The Service may allow you to submit, upload, or display content, including but not limited to text, images, and videos. You retain ownership of your content, but by submitting it to the Service, you grant [Your Company Name] a worldwide, royalty-free, non-exclusive, and sublicensable license to use, reproduce, modify, adapt, publish, distribute, and display your content in connection with the Service.
+  
+  You agree not to upload or share any content that is:
+  
+  Infringing on any third-party rights, including intellectual property rights.
+  Offensive, defamatory, or harmful to others.
+  Violating any applicable laws or regulations.
+  6. Copyright
+  
+  All content on the Service, including text, graphics, logos, and images, is the property of [Your Company Name] or its content suppliers and is protected by copyright laws. You may not use, reproduce, modify, or distribute any content from the Service without the express written consent of [Your Company Name].
+  
+  7. Termination
+  
+  [Your Company Name] reserves the right to terminate or suspend your access to the Service at its sole discretion, without notice, for any reason, including but not limited to a breach of these Terms.
+  
+  8. Disclaimer of Warranties
+  
+  The Service is provided "as is" and "as available" without any warranties of any kind, either express or implied, including but not limited to the implied warranties of merchantability, fitness for a particular purpose, or non-infringement. [Your Company Name] does not warrant that the Service will be error-free, uninterrupted, or secure.
+  
+  9. Limitation of Liability
+  
+  To the fullest extent permitted by law, [Your Company Name] shall not be liable for any indirect, incidental, special, consequential, or punitive damages, or any loss of profits or revenues, whether incurred directly or indirectly, or any loss of data, use, goodwill, or other intangible losses resulting from:
+  
+  Your use or inability to use the Service.
+  Any unauthorized access to or use of our servers and/or any personal information stored therein.
+  Any third-party content or conduct on the Service.
+  10. Governing Law
+  
+  These Terms are governed by and construed in accordance with the laws of [Your Jurisdiction], without regard to its conflict of law principles. Any disputes arising from or relating to these Terms or the Service shall be subject to the exclusive jurisdiction of the courts in [Your Jurisdiction].
+  
+  11. Contact Information
+  
+  If you have any questions or concerns about these Terms or the Service, please contact us at [Your Contact Email].
+`;
+
+export default termsAndAgreement
