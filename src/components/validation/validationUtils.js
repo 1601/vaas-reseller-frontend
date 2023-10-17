2952d2909a9f4c94db570fd6ff5f6ae2998ebd26
Rver17 <rvabasilio2019@plm.edu.ph>
2023-10-17 11:44:06 +0800

    created new validation components

---
+++ src/components/validation/validationUtils.js
@@ -0,0 +1,27 @@
+export const validateName = (name) => {
+    const nameRegex = /^[a-zA-Z-' ]+$/;
+    return nameRegex.test(name);
+  };
+  
+  export const validateEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return emailRegex.test(email);
+  };
+  
+  export const validateMobileNumber = (country, number, countryCodes, mobileNumberLengths) => {
+    const expectedLength = mobileNumberLengths[country];
+    const actualLength = number.length;
+  
+    if (!expectedLength) {
+      return 'Unsupported country';
+    }
+    if (actualLength < expectedLength) {
+      return 'Number is too short';
+    }
+    if (actualLength > expectedLength) {
+      return 'Number is too long';
+    }
+  
+    return '';
+  };
+  
