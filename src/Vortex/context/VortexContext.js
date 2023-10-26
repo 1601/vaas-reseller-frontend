90a03e804cc6a8cf444013de2f8718a3ea50057f
Rver17 <rvabasilio2019@plm.edu.ph>
2023-10-26 10:49:31 +0800

    fixed logic, added other logic

--- src/Vortex/context/VortexContext.js
+++ src/Vortex/context/VortexContext.js
@@ -1,97 +1,102 @@
-import React, { useState, useEffect } from "react"
-import { getVortexProducts } from "../../../api/public/vortex/product_service"
-import { getVortexTokenBase } from "../../../api/public/vortex/token_service"
-import socket from "../../../services/socketio"
-import SecureLS from "secure-ls"
+import React, { useState, useEffect } from 'react';
+import SecureLS from 'secure-ls';
+import { getVortexProducts } from '../../api/public/vortex/product_service';
+import { getVortexTokenBase } from '../../api/public/vortex/token_service';
+import socket from '../services/socketio';
 
-export const IsloadingProducts = React.createContext(false)
+export const IsloadingProducts = React.createContext(false);
 
-export const VortextProducts = React.createContext([])
+export const VortexProducts = React.createContext([]);
 
 export const VortexContextError = React.createContext({
   isError: false,
-  message: "",
-})
+  message: '',
+});
 
-export const ReloadProductsTrigger = React.createContext(false)
-
-
+export const ReloadProductsTrigger = React.createContext(false);
 
 const VortexContext = ({ children }) => {
+  const ls = new SecureLS({ encodingType: 'aes' });
 
-  const ls = new SecureLS({ encodingType: "aes" })
+  const [isLoading, setIsLoading] = useState(false);
 
-  const [isLoading, setIsLoading] = useState(false)
+  const [error, setErrorData] = useState({ isError: false, message: '' });
 
+  const [vortexProductData, setVortexProductData] = useState([]);
 
-
-  const [error, setErrorData] = useState({ isError: false, message: "" })
-
-  const [vortexProductData, setVortexProductData] = useState([])
-
-  const [retry, setRetry] = useState(false)
-
-
-
-
+  const [retry, setRetry] = useState(false);
 
   const loadVortexProducts = async () => {
-    setIsLoading(true)
-    let vortexTokenResponse = await getVortexTokenBase()
+    setIsLoading(true);
+    const vortexTokenResponse = await getVortexTokenBase();
+
+    console.log('Vortex Token Response:', vortexTokenResponse);
 
     if (vortexTokenResponse.status === 200) {
-      let vortextTokenResult = await vortexTokenResponse.json()
-      let response = await getVortexProducts(vortextTokenResult.access_token)
+      const vortexTokenResult = await vortexTokenResponse.json();
+      console.log('Vortex Token Result:', vortexTokenResult);
 
-      setIsLoading(false)
+      const response = await getVortexProducts(vortexTokenResult.access_token);
+      console.log('Vortex Products Response:', response);
+
+      setIsLoading(false);
       if (response.status === 200) {
-        let result = await response.json()
+        const result = await response.json();
+        console.log('Vortex Products Data:', result);
 
-        setVortexProductData(result)
-
+        setVortexProductData(result);
       } else {
-        let result = await response.json()
-        setIsLoading(false)
+        const result = await response.json();
+        console.log('Error in Vortex Products Response:', result);
+
+        setIsLoading(false);
         setErrorData({
           isError: true,
           message: result.error.message,
-        })
+        });
       }
     } else {
-      let vortextTokenResult = await vortexTokenResponse.json()
-      setIsLoading(false)
+      const vortexTokenResult = await vortexTokenResponse.json();
+      console.log('Error in Vortex Token Response:', vortexTokenResult);
+
+      setIsLoading(false);
       setErrorData({
         isError: true,
-        message: vortextTokenResult.error.message,
-      })
+        message: vortexTokenResult.error.message,
+      });
     }
-  }
-
+  };
 
   useEffect(() => {
-    loadVortexProducts()
+    loadVortexProducts();
+  }, [retry]);
 
-  }, [retry])
-
-
-
-
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100vh',
+          backgroundColor: '#fff',
+        }}
+      />
+    );
+  }
 
   return (
     <ReloadProductsTrigger.Provider value={[retry, setRetry]}>
       <VortexContextError.Provider value={[error, setErrorData]}>
         <IsloadingProducts.Provider value={[isLoading, setIsLoading]}>
-          <VortextProducts.Provider
-            value={[vortexProductData, setVortexProductData]}
-          >
-
+          <VortexProducts.Provider value={[vortexProductData, setVortexProductData]}>
             {children}
-
-          </VortextProducts.Provider>
+          </VortexProducts.Provider>
         </IsloadingProducts.Provider>
       </VortexContextError.Provider>
     </ReloadProductsTrigger.Provider>
-  )
-}
+  );
+};
 
-export default VortexContext
+export default VortexContext;
