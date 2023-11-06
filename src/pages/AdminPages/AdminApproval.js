b404c07f7e4426dc8176303d5919cd6cc3b21ac1
Rver17 <rvabasilio2019@plm.edu.ph>
2023-11-06 16:02:23 +0800

    added re-rendering after approval

--- src/pages/AdminPages/AdminApproval.js
+++ src/pages/AdminPages/AdminApproval.js
@@ -51,6 +51,7 @@ const AdminApproval = () => {
         setStoreDetails((prevStoreDetails) => ({
           ...prevStoreDetails,
           needsApproval: !isApproved,
+          isApproved,
         }));
       }
     } catch (error) {
