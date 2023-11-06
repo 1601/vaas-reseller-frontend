741365bba28dbb193bddfadcb47eb118f58e3c43
Rver17 <rvabasilio2019@plm.edu.ph>
2023-11-06 14:37:20 +0800

    removed comments

--- src/pages/StorePageEdit.js
+++ src/pages/StorePageEdit.js
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { ChromePicker } from 'react-color';
 import {
-  Box,
   Card,
   Grid,
   Container,
@@ -187,7 +186,6 @@ const StorePageEdit = () => {
     };
     setPlatformVariables(newPlatformVariables);
 
-    // Send only the individual field to be updated
     const requestBody = {
       [field]: newValue,
     };
@@ -365,7 +363,6 @@ const StorePageEdit = () => {
       const token = localStorage.getItem('token');
       const ownerId = JSON.parse(localStorage.getItem('user'))._id;
 
-      // Send the updated colors to the server
       const response = await axios.put(`${process.env.REACT_APP_BACKEND_URL}/api/updateColors/${ownerId}`, dataToSend, {
         headers: {
           'Content-Type': 'application/json',
@@ -379,7 +376,6 @@ const StorePageEdit = () => {
 
       alert('Colors updated successfully');
 
-      // Reload the webpage after a delay of 1 second (1000 milliseconds)
       setTimeout(() => {
         window.location.reload();
       }, 1000);
@@ -465,59 +461,6 @@ const StorePageEdit = () => {
           {storeData ? <>
             <div className="mb-4 w-full">
               <Card variant="outlined" style={{ padding: '20px', marginBottom: '20px' }}>
-                {/* <div
-                  style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}
-                 >
-                  <Typography variant="h4" gutterBottom>
-                    Store Details
-                  </Typography>
-                  <div >
-                    {isEditing ? (
-                      <div className="flex">
-                        <Button onClick={handleSaveClick} className="bg-blue-600 text-white px-4 py-2 rounded mr-2">
-                          Save
-                        </Button>
-                      </div>
-                    ) : (
-                      <>
-                        <Button
-                          variant="outlined"
-                          color="primary"
-                          className="mr-2"
-                          style={{ padding: '8px 16px', fontSize: '14px' }}
-                          onClick={handlePreviewClick}
-                        >
-                          Preview Store
-                        </Button>
-                        <Button onClick={handleEditClick} className="bg-blue-600 text-white px-4 py-2 rounded mr-2">
-                          Edit
-                        </Button>
-                        {storeData !== null && storeData.isApproved && !storeData.isLive && (
-                          <Button
-                            variant="contained"
-                            color="secondary"
-                            className="bg-green-600 text-white px-4 py-2 rounded"
-                            onClick={!storeData.kycApprove ? undefined : handleGoLiveClick}
-                            disabled={!storeData.kycApprove}
-                          >
-                            Go Live
-                          </Button>
-                        )}
-                        {storeData !== null && storeData.isLive && (
-                          <Button
-                            variant="contained"
-                            color="secondary"
-                            className="bg-red-600 text-white px-4 py-2 rounded"
-                            onClick={!storeData.kycApprove ? undefined : handleUnliveClick}
-                            disabled={!storeData.kycApprove}
-                          >
-                            Un-Live
-                          </Button>
-                        )}
-                      </>
-                    )}
-                  </div>
-                </div> */}
                 <div className="md:flex md:justify-between md:items-center mb-4">
                   <Typography variant="h4" gutterBottom className="w-full md:w-auto">
                     Store Details
@@ -682,20 +625,6 @@ const StorePageEdit = () => {
                           Add a default logo
                         </Typography>
                       </div>
-                      {/* <div>
-                        <input
-                          type="file"
-                          accept="image/png, image/jpeg"
-                          onChange={handleFileUpload}
-                          className="border p-2 rounded mt-2"
-                        />
-                        <Button
-                          onClick={() => handleUploadClick('logo')}
-                          className="bg-blue-600 text-white px-4 py-2 rounded mt-4"
-                        >
-                          Upload
-                        </Button>
-                      </div> */}
                       <div className="flex flex-col md:flex-row items-center space-y-2 md:space-x-2">
                         <input
                           type="file"
@@ -739,38 +668,6 @@ const StorePageEdit = () => {
                             marginBottom: '20px',
                           }}
                         >
-                          {/* Primary Color */}
-                          {/* <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
-                            <Typography variant="subtitle2" align="center">
-                              Primary Color
-                            </Typography>
-                            <ChromePicker
-                              color={color.primary.hex}
-                              onChangeComplete={(newColor) => setColor({ ...color, primary: { hex: newColor.hex } })}
-                            />
-                          </Box>
-
-                          Right side content 
-                          <Box ml={2} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
-                            {/* Current Primary Color
-                            <Typography variant="subtitle1" align="center">
-                              Current Primary Color
-                            </Typography>
-                            <Box
-                              width="100px"
-                              height="30px"
-                              bgcolor={storeData ? storeData.primaryColor : '#FFFFFF'}
-                              mb={2}
-                              border="1px solid #000"
-                            />
-
-                             New Primary Color 
-                            <Typography variant="subtitle1" align="center">
-                              New Primary Color
-                            </Typography>
-                            <Box width="100px" height="30px" bgcolor={color.primary.hex} border="1px solid #000" />
-                          </Box> */}
-
                           <div className="flex flex-col md:flex-row md:items-center md:space-x-4">
                             <div className="mb-4 md:mb-0 w-full md:w-auto">
                               <Typography variant="subtitle2" align="center">
@@ -781,8 +678,6 @@ const StorePageEdit = () => {
                                 onChangeComplete={(newColor) => setColor({ ...color, primary: { hex: newColor.hex } })}
                               />
                             </div>
-
-                            {/* Right side content */}
                             <div className="md:flex flex-col md:items-center">
                               {/* Current Primary Color */}
                               <div className="mb-4 md:mb-2">
@@ -817,37 +712,6 @@ const StorePageEdit = () => {
                             marginBottom: '20px',
                           }}
                         >
-                          {/* Secondary Color */}
-                          {/* <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
-                            <Typography variant="subtitle2" align="center">
-                              Secondary Color
-                            </Typography>
-                            <ChromePicker
-                              color={color.secondary.hex}
-                              onChangeComplete={(newColor) => setColor({ ...color, secondary: { hex: newColor.hex } })}
-                            />
-                          </Box> */}
-
-                          {/* Right side content */}
-                          {/* <Box ml={2} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
-                            {/* Current Secondary Color
-                            <Typography variant="subtitle1" align="center">
-                              Current Secondary Color
-                            </Typography>
-                            <Box
-                              width="100px"
-                              height="30px"
-                              bgcolor={storeData ? storeData.secondaryColor : '#FFF'}
-                              mb={2}
-                              border="1px solid #000"
-                            />
-
-                            {/* New Secondary Color
-                            <Typography variant="subtitle1" align="center">
-                              New Secondary Color
-                            </Typography>
-                            <Box width="100px" height="30px" bgcolor={color.secondary.hex} border="1px solid #000" />
-                          </Box> */}
                           <div className="flex flex-col md:flex-row md:items-center md:space-x-4">
                             <div className="mb-4 md:mb-0 w-full md:w-auto">
                               <Typography variant="subtitle2" align="center">
