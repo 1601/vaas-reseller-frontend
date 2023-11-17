a2892fc7d49445d637bf910b7667c3e396acd0e4
Rver17 <rvabasilio2019@plm.edu.ph>
2023-11-17 16:56:11 +0800

    added placeholder for default, mark-up, discount prices

--- src/pages/DealerPages/Products/TopUpProducts.js
+++ src/pages/DealerPages/Products/TopUpProducts.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Box, CardMedia, Switch, FormControlLabel, Grid, Paper, Typography } from '@mui/material';
+import { Box, Switch, FormControlLabel, Grid, Paper, Typography, TextField } from '@mui/material';
 import CircularLoading from '../../../components/preLoader';
 import TopUpImage from '../../../components/vortex/TopUpImage';
 
 const TopUpProducts = () => {
   const [topUpToggles, setTopUpToggles] = useState({
-    SMARTPH: true,
-    TNTPH: true,
-    PLDTPH: true,
-    GLOBE: true,
-    MERALCO: true,
-    CIGNAL: true,
+    SMARTPH: { enabled: true, defaultPrice: 'N/A', markup: '', discount: '' },
+    TNTPH: { enabled: true, defaultPrice: 'N/A', markup: '', discount: '' },
+    PLDTPH: { enabled: true, defaultPrice: 'N/A', markup: '', discount: '' },
+    GLOBE: { enabled: true, defaultPrice: 'N/A', markup: '', discount: '' },
+    MERALCO: { enabled: true, defaultPrice: 'N/A', markup: '', discount: '' },
+    CIGNAL: { enabled: true, defaultPrice: 'N/A', markup: '', discount: '' },
   });
   const [isLoading, setIsLoading] = useState(true);
 
   const token = localStorage.getItem('token');
-  const userId = JSON.parse(localStorage.getItem('user'))._id; 
+  const userId = JSON.parse(localStorage.getItem('user'))._id;
 
   useEffect(() => {
     axios
@@ -26,7 +26,8 @@ const TopUpProducts = () => {
         },
       })
       .then((response) => {
-        setTopUpToggles(response.data);
+        // Map response to your state structure
+        // setTopUpToggles(response.data);
         setIsLoading(false);
       })
       .catch((error) => {
@@ -37,15 +38,28 @@ const TopUpProducts = () => {
 
   const handleToggleChange = (event) => {
     const { name, checked } = event.target;
-    setTopUpToggles((prevState) => ({ ...prevState, [name]: checked }));
+    setTopUpToggles((prevState) => ({
+      ...prevState,
+      [name]: { ...prevState[name], enabled: checked },
+    }));
 
-    axios
-      .put(
-        `${process.env.REACT_APP_BACKEND_URL}/api/dealer/topup-toggles/${userId}`,
-        { topupToggles: { ...topUpToggles, [name]: checked } },
-        { headers: { Authorization: `Bearer ${token}` } }
-      )
-      .catch((error) => console.error('Error updating topup toggles:', error));
+    // Send update to backend here
+  };
+
+  const handleMarkupChange = (name, value) => {
+    // Update markup price for a product
+    setTopUpToggles((prevState) => ({
+      ...prevState,
+      [name]: { ...prevState[name], markup: value },
+    }));
+  };
+
+  const handleDiscountChange = (name, value) => {
+    // Update discount for a product
+    setTopUpToggles((prevState) => ({
+      ...prevState,
+      [name]: { ...prevState[name], discount: value },
+    }));
   };
 
   if (isLoading) {
@@ -59,14 +73,31 @@ const TopUpProducts = () => {
           Top-Up Products
         </Typography>
         <Grid container spacing={2}>
-          {Object.entries(topUpToggles).map(([key, value]) => (
+          {Object.entries(topUpToggles).map(([key, { enabled, defaultPrice, markup, discount }]) => (
             <Grid item xs={12} sm={6} md={4} key={key}>
               <Paper elevation={3} sx={{ padding: '10px', textAlign: 'center' }}>
                 <Typography variant="h6">{key}</Typography>
                 <TopUpImage title={key} />
                 <FormControlLabel
-                  control={<Switch checked={value} onChange={handleToggleChange} name={key} />}
-                  label={value ? 'Enabled' : 'Disabled'}
+                  control={<Switch checked={enabled} onChange={(e) => handleToggleChange(e)} name={key} />}
+                  label={enabled ? 'Enabled' : 'Disabled'}
+                />
+                <Typography>Default Price: {defaultPrice}</Typography>
+                <Typography>Mark-Up Price</Typography>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  value={markup}
+                  onChange={(e) => handleMarkupChange(key, e.target.value)}
+                  placeholder="Enter mark-up price"
+                />
+                <Typography>Discount</Typography>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  value={discount}
+                  onChange={(e) => handleDiscountChange(key, e.target.value)}
+                  placeholder="Enter discount"
                 />
               </Paper>
             </Grid>
