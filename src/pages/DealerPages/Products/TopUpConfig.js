d5bf1d461500ad9f2df556c752a3a1699d3fcef1
Rver17 <rvabasilio2019@plm.edu.ph>
2023-12-06 15:31:09 +0800

    added functionality for individual products toggle

--- src/pages/DealerPages/Products/TopUpConfig.js
+++ src/pages/DealerPages/Products/TopUpConfig.js
@@ -18,31 +18,65 @@ import {
 } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
-const TopUpConfig = ({ token }) => {
+const TopUpConfig = () => {
   const { productName } = useParams();
   const [productConfigs, setProductConfigs] = useState([]);
   const userId = JSON.parse(localStorage.getItem('user'))._id;
   const navigate = useNavigate();
+  const token = localStorage.getItem('token');
 
   useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_BACKEND_URL}/v1/api/dealer/product-config/${userId}/${productName}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((response) => {
-        // Sort products by defaultPrice
-        const sortedProducts = response.data.products.sort((a, b) => a.defaultPrice - b.defaultPrice);
-        setProductConfigs(sortedProducts);
-      })
-      .catch((error) => {
-        console.error('Error fetching product configurations:', error);
-      });
+    if (token) {
+      axios
+        .get(`${process.env.REACT_APP_BACKEND_URL}/v1/api/dealer/product-config/${userId}/${productName}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          const sortedProducts = response.data.products.sort((a, b) => a.defaultPrice - b.defaultPrice);
+          setProductConfigs(sortedProducts);
+        })
+        .catch((error) => {
+          console.error('Error fetching product configurations:', error);
+        });
+    }
   }, [userId, productName, token]);
 
   const handleToggle = (configId, enabled) => {
-    // Logic to handle toggle change
+    // Update local state
+    const updatedConfigs = productConfigs.map((config) => {
+      if (config._id === configId) {
+        return { ...config, enabled };
+      }
+      return config;
+    });
+    setProductConfigs(updatedConfigs);
+
+    // Prepare data for the API request
+    const updateData = {
+      enabled,
+    };
+
+    // Send update to backend for a specific product
+    if (token) {
+      axios
+        .put(
+          `${process.env.REACT_APP_BACKEND_URL}/v1/api/dealer/${userId}/topup-toggles/${productName}/${configId}`,
+          updateData,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((response) => {
+          console.log('Product updated successfully:', response.data);
+        })
+        .catch((error) => {
+          console.error('Error updating product:', error);
+        });
+    }
   };
 
   const handleMarkUpChange = (configId, event) => {
@@ -52,7 +86,7 @@ const TopUpConfig = ({ token }) => {
   const handleApplyDiscount = (configId) => {
     // Logic to apply discount
   };
-  
+
   return (
     <Box sx={{ padding: '20px' }}>
       <Paper elevation={3} sx={{ padding: '20px', margin: 'auto', maxWidth: '800px' }}>
@@ -77,31 +111,31 @@ const TopUpConfig = ({ token }) => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {productConfigs.map((config) => (
-                <TableRow key={config.id}>
-                  <TableCell>{config.name}</TableCell>
-                  <TableCell align="right">{config.defaultPrice}</TableCell>
-                  <TableCell align="right">
-                    <TextField
-                      variant="outlined"
-                      size="small"
-                      value={config.markup}
-                      onChange={(e) => handleMarkUpChange(config.id, e)}
-                    />
-                  </TableCell>
-                  <TableCell align="right">
-                    <Button variant="outlined" onClick={() => handleApplyDiscount(config.id)}>
-                      Apply
-                    </Button>
-                  </TableCell>
-                  <TableCell align="center">
-                    <Switch
-                      checked={config.enabled}
-                      onChange={() => handleToggle(config.id, !config.enabled)}
-                    />
-                  </TableCell>
-                </TableRow>
-              ))}
+              {productConfigs.map((config) => {
+                console.log('Config: ', config);
+                return (
+                  <TableRow key={config._id}>
+                    <TableCell>{config.name}</TableCell>
+                    <TableCell align="right">{config.defaultPrice}</TableCell>
+                    <TableCell align="right">
+                      <TextField
+                        variant="outlined"
+                        size="small"
+                        value={config.markup}
+                        onChange={(e) => handleMarkUpChange(config._id, e)}
+                      />
+                    </TableCell>
+                    <TableCell align="right">
+                      <Button variant="outlined" onClick={() => handleApplyDiscount(config._id)}>
+                        Apply
+                      </Button>
+                    </TableCell>
+                    <TableCell align="center">
+                      <Switch checked={config.enabled} onChange={() => handleToggle(config._id, !config.enabled)} />
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
             </TableBody>
           </Table>
         </TableContainer>
