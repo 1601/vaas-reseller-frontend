f60a13d4f718feb3fe2786a12f91be059599e61e
Rver17 <rvabasilio2019@plm.edu.ph>
2024-01-04 18:04:14 +0800

    added loginPage for admins

---
+++ src/src/pages/AuthPages/LoginPageAdmin.js
@@ -0,0 +1,198 @@
+import { Helmet } from 'react-helmet-async';
+import React, { useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
+import axios from 'axios';
+// @mui
+import { styled } from '@mui/material/styles';
+import { Link, Container, Typography, Divider, Stack, Button } from '@mui/material';
+// hooks
+import useResponsive from '../../hooks/useResponsive';
+// components
+import Logo from '../../components/logo';
+import Iconify from '../../components/iconify';
+// sections
+import LoginFormAdmin from '../../sections/auth/login/LoginFormAdmin'
+
+// ----------------------------------------------------------------------
+
+const StyledRoot = styled('div')(({ theme }) => ({
+  [theme.breakpoints.up('md')]: {
+    display: 'flex',
+  },
+}));
+
+const StyledSection = styled('div')(({ theme }) => ({
+  width: '100%',
+  maxWidth: 480,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  boxShadow: theme.customShadows.card,
+  backgroundColor: theme.palette.background.default,
+}));
+
+const StyledContent = styled('div')(({ theme }) => ({
+  maxWidth: 480,
+  margin: 'auto',
+  minHeight: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  padding: theme.spacing(12, 0),
+}));
+
+const StyledFooter = styled('div')({
+  alignSelf: 'flex-end',
+  mb: 2,
+});
+
+// ----------------------------------------------------------------------
+
+export default function LoginPage() {
+  const mdUp = useResponsive('up', 'md');
+  const navigate = useNavigate();
+  const location = useLocation();
+  const handleNavigation = (path) => {
+    navigate(path);
+  };
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const tokenFromURL = params.get('token');
+
+    if (tokenFromURL) {
+      localStorage.setItem('token', tokenFromURL);
+
+      // Extract email from the token
+      const decodedToken = jwtDecode(tokenFromURL);
+      const { email } = decodedToken;
+
+      // Fetch user details by email
+      axios
+        .get(`${process.env.REACT_APP_BACKEND_URL}/v1/api/dealer/email?email=${email}`)
+        .then((response) => {
+          const { firstName, lastName, testBalance, accountBalance, accountStatus, _id } = response.data;
+
+          // Save user details in localStorage as 'user'
+          localStorage.setItem(
+            'user',
+            JSON.stringify({
+              _id,
+              firstName,
+              lastName,
+              email,
+              testBalance,
+              accountBalance,
+              accountStatus,
+            })
+          );
+
+          // Navigate to the desired page (e.g., dashboard)
+          navigate('/dashboard/app');
+        })
+        .catch((error) => {
+          console.error('Error fetching user details:', error);
+        });
+    }
+  }, [location, navigate]);
+
+  const handleGoogleLogin = () => {
+    const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+    const redirectUri = process.env.REACT_APP_GOOGLE_REDIRECT_URI;
+    const scope = encodeURIComponent(
+      'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
+    );
+
+    const state = encodeURIComponent(window.location.origin);
+
+    const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?scope=${scope}&access_type=offline&include_granted_scopes=true&redirect_uri=${redirectUri}&response_type=code&client_id=${clientId}&state=${state}`;
+
+    window.location.href = googleLoginUrl;
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title> Admin Login | VAAS </title>
+      </Helmet>
+
+      <StyledRoot>
+        {/* {mdUp && (
+          <StyledSection>
+            <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
+              Hi, Welcome Back
+            </Typography>
+            <img src="/assets/illustrations/illustration_login.png" alt="login" />
+          </StyledSection>
+        )} */}
+
+        <Container maxWidth="sm" sx={{ backgroundColor: '#fff' }}>
+          <Logo
+            sx={{
+              alignSelf: 'center',
+              // position: 'fixed',
+              // top: { xs: 16, sm: 24, md: 40 },
+              // left: { xs: 16, sm: 24, md: 40 },
+            }}
+          />
+          <StyledContent>
+            <Typography variant="h4" gutterBottom>
+              Admin Login
+            </Typography>
+
+            <Typography variant="body2" sx={{ mb: 5 }}>
+              Continue to Vortex Admin {''}
+              {/* <Link variant="subtitle2">Get started</Link> */}
+            </Typography>
+
+            <LoginFormAdmin />
+
+            {/* <Divider sx={{ my: 3 }}>
+              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                OR
+              </Typography>
+            </Divider> */}
+
+            <Stack direction="column" spacing={2}>
+              
+              {/* <Button fullWidth size="large" color="inherit" variant="outlined" onClick={handleGoogleLogin}>
+                <Iconify icon="eva:google-fill" color="#DF3E30" width={22} height={22} />
+                Continue with Google
+              </Button>
+
+              <Button fullWidth size="large" color="inherit" variant="outlined">
+                <Iconify icon="eva:facebook-fill" color="#1877F2" width={22} height={22} />
+                Continue with Facebook
+              </Button>
+
+              {/* <Button fullWidth size="large" color="inherit" variant="outlined">
+                <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={22} height={22} />
+              </Button> */}
+            </Stack>
+
+            {/* <Typography variant="body2" sx={{ mt: 0.25 }}>
+              Not yet Verified?
+              <Link variant="subtitle2" onClick={() => navigate('/verify')} sx={{ cursor: 'pointer', ml: 1 }}>
+                Verify
+              </Link>
+            </Typography> */}
+            <Typography variant="body2" sx={{ textAlign: 'center', mt: 2 }}>
+              <Link variant="subtitle2" component="button" onClick={() => handleNavigation('/help')}>
+                Help
+              </Link>{' '}
+              {' | '}
+              <Link variant="subtitle2" component="button" onClick={() => handleNavigation('/data-privacy-policy')}>
+                Privacy
+              </Link>{' '}
+              {' | '}
+              <Link variant="subtitle2" component="button" onClick={() => handleNavigation('/terms-and-conditions')}>
+                Terms of Service
+              </Link>
+            </Typography>
+          </StyledContent>
+        </Container>
+      </StyledRoot>
+    </>
+  );
+}
---
+++ src/src/sections/auth/login/LoginFormAdmin.js
@@ -0,0 +1,256 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import SecureLS from 'secure-ls';
+// @mui
+import {
+  Button,
+  Link,
+  Stack,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Checkbox,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  CircularProgress,
+} from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+// components
+import Iconify from '../../../components/iconify';
+import defaultProductConfig from './default_product_config.json';
+
+// ----------------------------------------------------------------------
+
+const ls = new SecureLS({ encodingType: 'aes' });
+
+export default function LoginForm() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [verificationMessage, setVerificationMessage] = useState('');
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+  const [loggingIn, setLoggingIn] = useState(false);
+
+  useEffect(() => {
+    try {
+      const savedEmail = ls.get('rememberMeEmail');
+      const savedPassword = ls.get('rememberMePassword');
+      if (savedEmail && savedPassword) {
+        setEmail(savedEmail);
+        setPassword(savedPassword);
+        setRememberMe(true);
+      }
+    } catch (error) {
+      console.error('Error reading from SecureLS:', error);
+    }
+  }, []);
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    return emailRegex.test(email);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+  };
+
+  useEffect(() => {
+    if (email.trim()) setEmailError(false);
+  }, [email]);
+
+  useEffect(() => {
+    if (password.trim()) setPasswordError(false);
+  }, [password]);
+
+  const handleRememberMeChange = (e) => {
+    setRememberMe(e.target.checked);
+  };
+
+  const handleLogin = async () => {
+    setLoggingIn(true);
+    setError('');
+    if (!email.trim() || !password.trim()) {
+      setError('Please supply all required fields');
+      setEmailError(!email.trim());
+      setPasswordError(!password.trim());
+      setDialogOpen(true);
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setEmailError(true);
+      setVerificationMessage('Invalid email format');
+      setDialogOpen(true);
+      return;
+    }
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/v1/api/auth/login`, {
+        email,
+        password,
+      });
+
+      const { token, role } = response.data;
+
+      if (rememberMe) {
+        ls.set('rememberMeEmail', email);
+        ls.set('rememberMePassword', password);
+        ls.set('rememberMe', 'true');
+      }
+
+      if (role !== 'admin') {
+        setError('Dealers must login through the dealer portal');
+        setDialogOpen(true);
+        setLoggingIn(false);
+        return;
+      }
+
+      const verifiedRole = await verifyRole(token);
+
+      if (verifiedRole) {
+        localStorage.setItem('role', verifiedRole);
+      } else {
+        console.error('No role received from verifyRole API');
+      }
+
+      localStorage.setItem('token', token);
+      localStorage.setItem('user', JSON.stringify(response.data));
+
+      navigate(role === 'admin' ? '/dashboard/admin' : '/dashboard/app', { replace: true });
+      setLoggingIn(false);
+    } catch (error) {
+      if (error.response && error.response.data) {
+        setError(error.response.data.message);
+      } else {
+        setError('Invalid email or password');
+      }
+      setDialogOpen(true);
+      setLoggingIn(false);
+    }
+  };
+
+  const verifyRole = async (token) => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/v1/api/auth/role`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return response.data.role;
+    } catch (error) {
+      console.error('Could not verify user role', error);
+      return null;
+    }
+  };
+
+  return (
+    <>
+      <Stack spacing={3}>
+        <TextField
+          error={emailError || !!verificationMessage}
+          name="email"
+          label="Email address"
+          value={email}
+          onChange={handleEmailChange}
+          helperText={emailErrorMessage || verificationMessage}
+        />
+        <TextField
+          error={passwordError}
+          name="password"
+          label="Password"
+          type={showPassword ? 'text' : 'password'}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          helperText={passwordError && 'Password is required'}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                  <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Stack>
+      <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Checkbox name="remember" checked={rememberMe} onChange={handleRememberMeChange} />
+          <Typography variant="body2">Remember Me</Typography>
+        </div>
+        <Link
+          variant="subtitle2"
+          underline="hover"
+          onClick={() => navigate('/forgotpassword')}
+          sx={{ cursor: 'pointer' }}
+        >
+          Forgot password?
+        </Link>
+      </Stack>
+      <Button
+        fullWidth
+        size="large"
+        color="inherit"
+        variant="outlined"
+        onClick={() => {
+          handleLogin();
+          if (rememberMe) {
+            ls.set('rememberMe', 'true');
+          } else {
+            ls.remove('rememberMe');
+            ls.remove('rememberMeEmail');
+            ls.remove('rememberMePassword');
+          }
+        }}
+      >
+        Login
+      </Button>
+      {error && (
+        <Typography variant="body2" color="error" sx={{ mt: 2 }}>
+          {error}
+        </Typography>
+      )}
+      <Dialog open={dialogOpen} onClose={handleCloseDialog}>
+        <DialogTitle>{error ? 'Error' : 'Notice'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{error || verificationMessage}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog open={loggingIn || dialogOpen} onClose={handleCloseDialog}>
+        <DialogTitle>{error ? 'Error' : loggingIn ? 'Logging in...' : 'Notice'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {loggingIn ? 'Please wait while we log you in...' : error || verificationMessage}
+          </DialogContentText>
+          {loggingIn && <CircularProgress />}
+        </DialogContent>
+        {!loggingIn && (
+          <DialogActions>
+            <Button onClick={handleCloseDialog} color="primary">
+              Close
+            </Button>
+          </DialogActions>
+        )}
+      </Dialog>
+    </>
+  );
+}
